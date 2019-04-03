effc17dd29465bfa792ed00e0b8e81e50e107355
Field Ruan <fieldruan@gmail.com>
2019-04-03 09:30:33 +0800

    Merge master and Fieldruan

--- hello.c
+++ hello.c
@@ -1,8 +1,27 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void InsertionSort(int *a, int n);
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello World!");
+    int a[] = {5,7,3,2,8,11,33,27,21,89,17};
+
+    printf("Hello world!");
     return 0;
+}
+
+void InsertionSort(int *a, int n){
+    int i,j,key;
+    for( j = 1; j < n; j++)
+    {
+        key = a[j];
+        i = j-1;
+        while(i>=0 && a[i] > key){
+            a[i+1] = a[i];
+            i--;
+        }
+        a[i+1] = key;
+    }
+    
 }
