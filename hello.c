379879a3c4d2636bde8a3c197e11e71f7103630f
Field Ruan <fieldruan@gmail.com>
2019-04-03 09:08:13 +0800

    InsertionSort

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void InsertionSort(int *a, int n);
+
 int main(int argc, char const *argv[])
 {
     int a[] = {5,7,3,2,8,11,33,27,21,89,17};
@@ -9,3 +11,17 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
+void InsertionSort(int *a, int n){
+    int i,j,key;
+    for( j = 1; j < n; j++)
+    {
+        key = a[j];
+        i = j-1;
+        while(i>=0 && a[i] > key){
+            a[i+1] = a[i];
+            i--;
+        }
+        a[i+1] = key;
+    }
+    
+}
