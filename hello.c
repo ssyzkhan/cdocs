a30316e012eda7b151eabc815e09c4e8369571db
Field Ruan <fieldruan@gmail.com>
2019-04-03 08:53:34 +0800

    check out Fieldruan

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int a[] = {5,7,3,2,8,11,33,27,21,89,17};
+
+    printf("Hello world!");
+    return 0;
+}
+
